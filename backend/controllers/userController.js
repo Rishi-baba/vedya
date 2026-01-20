c0fdbc74c787db5daf8bf8e793db478ec2394d1c
risibaba <rishibankar16@gmail.com>
2026-01-21 05:12:00 +0530

    admin ka bla bla

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -89,6 +89,23 @@ const registerUser = async (req,res)=>{
 // Route for admin login
 const adminLogin = async (req,res)=>{
 
+  try {
+    
+    const {email,password} = req.body
+
+    if(email === process.env.AdMIN_EMAIL && password === process.env.AdMIN_PASSWORD){
+      const token = jwt.sign(email+password,process.env.JWT_SECRET)
+      res.json({success:true,token})
+    }
+    else{
+      res.json({success:false, message:"Invalid credentials"})
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.json({success:false,message:error.message})
+  }
+
 }
 
 
