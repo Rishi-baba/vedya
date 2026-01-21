b823d151984bac736a6ab25ff48ba4b26ef757ed
risibaba <rishibankar16@gmail.com>
2026-01-21 19:35:08 +0530

    admin list page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Order from "./pages/Order";
 import Login from "./components/Login";
 import { ToastContainer } from "react-toastify";
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
-
+export const currency = '₹'
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
 
